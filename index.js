ca0fa92b9324308343f37ce1561f16666d511ff6
DrashtiBaldha <drashtibaldha2003@gmail.com>
2025-07-18 09:23:57 +0530

    1 to 6 program

---
+++ index.js
@@ -0,0 +1,66 @@
+
+// const name="Drashti"
+// console.log("hello "+name)
+
+//1
+// console.log("1st Program")
+// const n=5
+// if(n%2)
+// {
+//     console.log("Odd number")
+// }
+// else
+// {
+//     console.log("Even number")
+// }
+
+//2
+// const a=10
+// const b=15
+// if(a>b)
+// {
+//     console.log("a is max")
+// }
+// else
+// {
+//     console.log("b is max")
+// }
+
+//3
+// const n=2024
+// if(n%4)
+// {
+//     console.log("Non leap year")
+// }
+// else
+// {
+//     console.log("Leap year")
+// }
+
+//4
+// const n=5
+// let sum=0
+// for(i=1;i<=n;i++)
+// {
+//     sum+=i
+// }
+// console.log(sum)
+
+//5
+// const n=5
+// let fact=1
+// for(i=1;i<=n;i++)
+// {
+//     fact*=i
+// }
+// console.log(fact)
+
+//6
+// const n=5
+// for(i=1;i<=10;i++)
+// {
+//     console.log(`${n} x ${i} = ${n*i}`)
+// }
+
+//7
+const num=
